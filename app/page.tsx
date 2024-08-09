c0ac2fed9baf36bf606cdabcc57015100e4a7ae2
mac <mac@MacBook-Air-de-mac.local>
2024-08-09 17:19:44 +0100

    Finishing the footer

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { navItems } from "@/data";
 import Clients from "./components/Clients";
 import Experience from "./components/Experience";
 import Approach from "./components/Approach";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
