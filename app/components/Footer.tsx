a59c9a18191857f58f5f2391f19351c793147bc5
mac <mac@MacBook-Air-de-mac.local>
2024-08-10 19:15:32 +0100

    Fixing up some bugs

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     <span className='text-purple'> your </span>
                     digital presence to the next level?
                 </h1>
-                <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let's discuss how I can help you achieve your goals.</p>
+                <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
                 <a href="mailto:affi.amin.work@gmail.com">
                     <MagicButton 
                         title="Let's get in touch"
@@ -27,7 +27,7 @@ const Footer = () => {
                 <div className='flex items-center md:gap-3 gap-6'>
                     {socialMedia.map((profile) => (
                         <div key={profile.id} className='w-10 h-10 cursor-pointer flex items-center justify-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                            <img src={profile.img} alt={profile.id} width={20} height={20}/>
+                            <img src={profile.img} width={20} height={20}/>
                         </div>
                     ))}
                 </div>
